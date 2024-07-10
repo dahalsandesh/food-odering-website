59e2f4943998c5f39ce62cd6b5d5a049c8572698
Dahal Sandesh <dahalsandesh55@gmail.com>
2024-07-10 22:33:39 +0545

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <section class="footer">
         <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="https://www.pankajshah12.com.np">Pankaj shah</a></p>
+            <p>All rights reserved. Designed By <a href="https://www.dahalsandesh.com.np">Dahal Sandesh</a></p>
         </div>
     </section>
     <!-- footer Section Ends Here -->
